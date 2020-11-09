9a112eb87a08e53ecaa889b103a8af6a37ceb34b
Anton Levin <holeinbrain@gmail.com>
2020-11-09 20:17:59 +0200

    lol

---
+++ Simple Counter/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Simple Counter
+//
+//  Created by Anton Levin on 09.11.2020.
+//  Copyright © 2020 Anton Levin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+ 
+        
+    }
+
+
+}
+
