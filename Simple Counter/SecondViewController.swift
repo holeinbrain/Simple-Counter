55ae9cdcac5dd3f5cf5dcc33a0a5d87170066998
Anton Levin <holeinbrain@gmail.com>
2020-11-10 08:27:20 +0200

    Add auto layout Fix several bugs

--- Simple Counter/SecondViewController.swift
+++ Simple Counter/SecondViewController.swift
@@ -9,14 +9,49 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    var firstCount = 0
+    var secondCount = 0
+    
+    @IBOutlet weak var firstLable: UILabel!
+    
+    @IBOutlet weak var secondLable: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    @IBAction func firstPlus(_ sender: Any) {
+        firstCount += 1
+        firstLable.text = "\(firstCount)"
+    }
     
-
+    
+    @IBAction func secondPlus(_ sender: Any) {
+        secondCount += 1
+        secondLable.text = "\(secondCount)"
+    }
+    
+    @IBAction func firstMinus(_ sender: Any) {
+        if firstCount <= 0 {
+                return firstCount = 0
+            } else {
+            firstCount -= 1
+            firstLable.text = "\(firstCount)"
+            }
+    }
+    
+    @IBAction func secondMinus(_ sender: Any) {
+        if secondCount <= 0 {
+                return secondCount = 0
+            } else {
+            secondCount -= 1
+            secondLable.text = "\(secondCount)"
+            }
+    }
+    @IBAction func shareResults(_ sender: Any) {
+    }
+    
     /*
     // MARK: - Navigation
 
--- Simple Counter/ViewController.swift
+++ Simple Counter/ViewController.swift
@@ -8,14 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController {
+    
+    
+ 
+    var count = 0
+    @IBOutlet weak var lable: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
  
-        
     }
 
-
+    @IBAction func unwindowToViewController (sender: UIStoryboardSegue) {
+       
+    }
+    
+    @IBAction func shareButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction func plusButton(_ sender: Any) {
+        count += 1
+        lable.text = "\(count)"
+        
+    }
+    @IBAction func minusButton(_ sender: Any) {
+        if count <= 0 {
+            return count = 0
+        } else {
+        count -= 1
+        lable.text = "\(count)"
+        }
+    }
 }
 
